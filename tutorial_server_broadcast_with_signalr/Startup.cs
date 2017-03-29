81c314ee6602d9845bdffe0a0878207c52154c29
Boris Town <tbziy@hotmail.com>
2017-03-29 11:57:24 +0800

    添加项目文件。

---
+++ tutorial_server_broadcast_with_signalr/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(tutorial_server_broadcast_with_signalr.Startup))]
+
+namespace tutorial_server_broadcast_with_signalr
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            // Any connection or hub wire up and configuration should go here
+            app.MapSignalR();
+        }
+    }
+}
---
+++ tutorial_server_broadcast_with_signalr/StockTickerHub.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace tutorial_server_broadcast_with_signalr
+{
+    [HubName("stockTickerMini")]
+    public class StockTickerHub : Hub
+    {
+        private readonly StockTicker _stockTicker;
+
+        public StockTickerHub() : this(StockTicker.Instance) { }
+
+        public StockTickerHub(StockTicker stockTicker)
+        {
+            _stockTicker = stockTicker;
+        }
+
+        public IEnumerable<Stock> GetAllStocks()
+        {
+            return _stockTicker.GetAllStocks();
+        }
+
+    }
+}
